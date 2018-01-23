62a01e0a36afd33799343f75458e35ecc4ded8ed
Qian Li <qianli1987@arabidopsis.org>
2018-01-22 16:22:28 -0800

    PWL-536 typo fix

--- proxy/src/org/phoenixbioinformatics/api/ApiService.java
+++ proxy/src/org/phoenixbioinformatics/api/ApiService.java
@@ -159,7 +159,7 @@ public class ApiService extends AbstractApiService {
     format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ssZ");
     String pageViewDate = format.format(curDate);
 
-    List<org.omg.CORBA.NameValuePair> params = new ArrayList<NameValuePair>(2);
+    List<NameValuePair> params = new ArrayList<NameValuePair>(2);
     params.add(new BasicNameValuePair("pageViewDate", pageViewDate));
     params.add(new BasicNameValuePair("uri", uri));
     params.add(new BasicNameValuePair("sessionId", sessionId));
